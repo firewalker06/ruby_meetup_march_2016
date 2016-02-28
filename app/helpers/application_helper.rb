bd78fdfbc25894ccc6029597277f342b98b4702f
firewalker06 <didik.wicaksono@gmail.com>
2016-02-29 00:56:09 +0700

    Add examples

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def icon(name, options = {})
+    options[:class] = "icon icon_#{name} #{options[:class]}"
+    text = options.delete(:text)
+    str = content_tag :i, nil, options
+    str += " #{text}" if text
+    str
+  end
 end
